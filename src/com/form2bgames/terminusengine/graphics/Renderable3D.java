06693f821d0e42ebb7ebc6353c550278924827b2
Nicholas Johnson <johnnic431@gmail.com>
2017-05-31 15:32:09 -0500

    Commiting to github

--- src/com/form2bgames/terminusengine/graphics/Renderable3D.java
+++ src/com/form2bgames/terminusengine/graphics/Renderable3D.java
@@ -1,7 +1,7 @@
 package com.form2bgames.terminusengine.graphics;
 
 public class Renderable3D implements AutoCloseable{
-
+	public VAO3D vaos[];
 	@Override
 	public void close() throws Exception {
 		// TODO Auto-generated method stub
